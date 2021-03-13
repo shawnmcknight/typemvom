272a35246b944f37b632a38655324ef1d03e0299
Shawn McKnight <smcknight@storis.com>
2021-03-13 12:56:46 -0500

    WIP

--- packages/core/.babelrc.js
+++ packages/core/.babelrc.js
@@ -1,6 +1,22 @@
 module.exports = {
 	ignore: ['**/*.d.ts'],
-	presets: [['@babel/preset-env', { targets: { node: true } }], '@babel/typescript'],
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: { node: '10.0' },
+				useBuiltIns: 'usage',
+				corejs: { version: 3 },
+				shippedProposals: true,
+			},
+		],
+		'@babel/typescript',
+	],
+	plugins: [
+		'babel-plugin-transform-typescript-metadata',
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		['@babel/proposal-class-properties', { loose: true }],
+	],
 	env: {
 		build: {
 			ignore: ['**/*.test.ts', '**/*.test.tsx', '**/__tests__/**'], // do not build test files
